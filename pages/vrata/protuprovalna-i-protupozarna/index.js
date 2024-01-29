2fc42c6f476152494808117a3483ec4e9814c972
Kreya <crashaw@gmail.com>
2024-01-29 11:54:37 +0100

    dodan sadrzaj na 2 podstranice

--- pages/vrata/protuprovalna-i-protupozarna/index.js
+++ pages/vrata/protuprovalna-i-protupozarna/index.js
@@ -4,6 +4,10 @@ import SectionColor from "../../../components/SectionColor";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import ImageCard from "../../../components/ImageCard";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { INLINES, BLOCKS } from "@contentful/rich-text-types";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function ProtuprovalnaIProtupozarnaStranica({
   stranica,
@@ -12,6 +16,32 @@ export default function ProtuprovalnaIProtupozarnaStranica({
   const { naslovHero, podnaslovHero, slikaHero, slikaVieslojni, slikaMasivni } =
     stranica;
   const router = useRouter();
+  const options = {
+    renderNode: {
+      [INLINES.HYPERLINK]: (node, children) => {
+        return (
+          <Link href={node.data.uri}>
+            <a>{children}</a>
+          </Link>
+        );
+      },
+    },
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+        return (
+          <div className="flex justify-center items-center my-2">
+            <Image
+              src={`https:${node.data.target.fields.file.url}`}
+              height={300}
+              width={500}
+              className="rounded-lg"
+              alt={node.data.target.fields.title}
+            />
+          </div>
+        );
+      },
+    },
+  };
 
   return (
     <>
@@ -38,6 +68,13 @@ export default function ProtuprovalnaIProtupozarnaStranica({
                 slika={`https:${podstranica.fields.slikaCard.fields.file.url}`}
               />
             ))}
+        </div>
+      </SectionColor>
+      <SectionColor>
+        <div className="flex flex-col">
+          <div className="prose md:leading-7 prose-p:my-0 prose-headings:my-6 max-w-none prose-p:text-justify text-lg prose-headings:text-zuta text-white">
+            {documentToReactComponents(sadrzaj, options)}
+          </div>
         </div>
       </SectionColor>
     </>
